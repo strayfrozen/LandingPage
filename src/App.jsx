393c5e3c968d233207e750b7daade03e9568c878
stray <strayfrozen@yahoo.com>
2024-02-20 20:07:36 -0600

    fixed bug with data file also populated all Core concepts with data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { CORE_CONCEPTS } from './data.js';
 
-import componentsImg from './assets/components.png';
+
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -38,13 +39,26 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept title="Components" 
-          description="The core UI building block."
-          image={componentsImg}
+          <CoreConcept
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
            />
-          <CoreConcept />
-          <CoreConcept />
-          <CoreConcept />
+          <CoreConcept 
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            image={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcept 
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            image={CORE_CONCEPTS[2].image}
+            />
+          <CoreConcept 
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+          />
         </ul>
         </section>
       </main>
